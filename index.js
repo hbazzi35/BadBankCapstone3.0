768a1405ce1bad1464dcac2379c073f8d9678d04
Hassan Bazzi <Bazzih3519@gmail.com>
2022-07-04 03:44:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.static('public'));
 app.use(cors());
 
-const uri = "mongodb+srv://bishalpandit:RuLdJrrHtGCpkXR2@cluster0.flu4x.mongodb.net/delfoody?retryWrites=true&w=majority";
+const uri = "mongodb+srv://bazzih3519:Bazzi3519@badbank2.wdzpz.mongodb.net/?retryWrites=true&w=majority";
 //"mongodb+srv://bishalpandit:RuLdJrrHtGCpkXR2@cluster0.flu4x.mongodb.net/delfoody?retryWrites=true&w=majority"
 async function connect() {
     try {
